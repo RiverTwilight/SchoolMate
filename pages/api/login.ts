dc0f20baa8e9c4bc89a6f0ade6b8e80882190e39
RiverTwilight <wrj2014@126.com>
2021-03-25 14:23:35 +0000

    feat: login api

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,16 +1,39 @@
 import sha256 from "crypto-js/sha256"
+import sql from "../../utils/db"
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default (req, res) => {
+type Data = {
+    message: string;
+    token?: string
+}
+
+/**
+ * 登录
+ */
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     const { name, tel } = req.query;
-    
-    res.status(200).json({ 
+
+    const data = await sql.get("user", ["*"], {
+        where: {
+            name,
+            tel
+        }
+    })
+
+    if (!!data.length) {
+        res.status(200).json({
+            message: "用户不存在"
+        });
+    }
+
+    res.status(200).json({
         message: "登录成功",
         token: generateToken(name, tel),
-     });
+    });
 };
 
 const generateToken = (
-    name: string, tel
+    name: string, tel: string
 ): string => {
     return sha256(name + tel)
 }
