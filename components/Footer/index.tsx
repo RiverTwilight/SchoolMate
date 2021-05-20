5d1e6ef65cfcb40e3d05096e3f60e8aaf7fbadb9
RiverTwilight <wrj2014@126.com>
2021-05-20 10:23:55 +0000

    style: add footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,18 +1,25 @@
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
 import clsx from "clsx";
 
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		footer: {
 			"--flow-space": "0.75rem",
-			backgroundColor: "#888",
+			// backgroundColor: "#888",
+			minHeight: "120px"
 		},
 	})
 );
 
 const Footer = () => {
 	const classes = useStyles();
-	return <footer className={classes.footer}>sadf</footer>;
+	return (
+		<footer className={classes.footer}>
+			<Divider />
+		sadf
+		</footer>
+	);
 };
 
 export default Footer;
